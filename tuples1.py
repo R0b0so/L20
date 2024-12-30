2d99de43585cf7e0912113131925f17cbc9761d7
R0b0so <ethansagar222@gmail.com>
2024-12-29 18:40:06 -0800

    hello

---
+++ tuples1.py
@@ -0,0 +1,13 @@
+tuplex = ("tuples", True, 2, 4)
+print(tuplex)
+tuplex = (5, 6, 7, 8, 4, 1)
+print(tuplex)
+tuplex = tuplex + (10,)
+print(tuplex)
+tuple1 = (3, ("Hello", "World",), [7, 9, "LIST"])
+print(tuple1)
+_slice = tuple1[1:-1]
+print(_slice)
+_slice = tuple1[-1:1]
+print(_slice)
+
